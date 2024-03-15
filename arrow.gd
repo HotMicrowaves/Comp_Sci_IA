68b5e5caa6723c5d3acf92d721f7e1c4557f1c1d
HotMicrowaves <119889673+HotMicrowaves@users.noreply.github.com>
2024-03-15 02:56:40 -0500

    Add files via upload

---
+++ arrow.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@onready var animation = get_node("AnimatedSprite2D")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation.play("Idle")
+
+
